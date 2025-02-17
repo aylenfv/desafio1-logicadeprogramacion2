5cc25c2637c70f08b4e573a5deb3d3881e5ce195
Aylenfv <fissoreaylen@gmail.com>
2025-02-17 16:31:05 -0300

    funcion asociada a botón con ingreso de datos por prompt y salida por alert con template

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ etiquetaH1.innerHTML = "Hora del desafío";
 function mostrarMensaje () {
     console.log("El botón fue clicado");
 }
+
+function mostrarCiudad() {
+    ciudad = prompt("Ingresa el nombre de una ciudad de brasil: ")
+    alert(`Estuve en ${ciudad} y me acordé de ti.`)
+}

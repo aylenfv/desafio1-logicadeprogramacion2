e408bda5f094a1358b8bc476135f1b8dcdf1af13
Aylenfv <fissoreaylen@gmail.com>
2025-02-17 16:40:07 -0300

    función suma

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ function mostrarCiudad() {
 function mensaje (){
     alert("Yo amo JS");
 }
+
+function suma(){
+    num1 = parseInt(prompt("Ingresa el primer número a sumar: "));
+    num2 = parseInt(prompt("Ingresa el segundo número: "));
+    num = num1+ num2;
+    alert(`La suma de ${num1} y ${num2} es ${num}`)
+}
